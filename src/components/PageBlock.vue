a016ead4cb86b180a84d1de7a717b87a67c4795b
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-05-10 16:05:02 +0200

    decode tx into hash and query

--- src/components/PageBlock.vue
+++ src/components/PageBlock.vue
@@ -30,13 +30,16 @@ page(:title="`Block ${block.header.height}`")
     :dd="p.signature.data")
 
   part(title='Transactions')
-    list-item(v-for="tx in block.data.txs" :key="tx.id"
-      dt="Transaction" :dd="TODO")
+  part(v-for="tx in block.data.txs" :title="tx.hash" :key="tx.hash")
+    list-item(v-for="(tx, key) in tx" :key="tx.hash + 'key'" :dt="key" :dd="tx")
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import axios from 'axios'
+import createHash from 'create-hash'
+import varint from 'varint'
+import b64 from 'base64-js'
 import ToolBar from './NiToolBar'
 import ListItem from './NiListItem'
 import Part from './NiPart'
@@ -135,6 +138,34 @@ export default {
       let json = await axios.get(url)
       this.block_meta = json.data.result.block_meta
       this.block = json.data.result.block
+      this.block.data.txs && this.queryTxs().catch((err) => console.log(err))
+    },
+    queryTxs () {
+      return this.queryTx(this.block.data.txs.length)
+    },
+    queryTx (len, key = 0) {
+      return new Promise((resolve, reject) => {
+        if (key >= len) resolve()
+        let txstring = atob(this.block.data.txs[key])
+        // console.log(txstring)
+        let txbytes = b64.toByteArray(this.block.data.txs[key])
+        // console.log(txbytes)
+        let varintlen = new Uint8Array(varint.encode(txbytes.length))
+        // console.log(varintlen)
+        let tmp = new Uint8Array(varintlen.byteLength + txbytes.byteLength)
+        tmp.set(new Uint8Array(varintlen), 0)
+        tmp.set(new Uint8Array(txbytes), varintlen.byteLength)
+        // console.log(tmp)
+        let hash = createHash('ripemd160').update(Buffer.from(tmp)).digest('hex')
+        // console.log(hash)
+        let url = `${this.bc.url}/tx?hash=0x${hash}`
+        axios.get(url).then(json => {
+          // console.log(json)
+          json.data.result.string = txstring
+          this.block.data.txs.splice(key, 1, json.data.result)
+          this.queryTx(len, key + 1).then(resolve).catch(reject)
+        }).catch(reject)
+      })
     }
   },
   async mounted () {
