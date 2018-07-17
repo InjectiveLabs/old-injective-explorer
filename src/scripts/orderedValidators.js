711247ef6581625be15fd4a6b6df4f7bfe627912
Peng Zhong <172531+nylira@users.noreply.github.com>
2018-07-17 15:22:17 +0800

    show validators with avatars first

--- src/scripts/orderedValidators.js
+++ src/scripts/orderedValidators.js
@@ -1,12 +1,21 @@
 import { orderBy } from "lodash"
 export default function(validators) {
+  let validatorsWithAvatars = []
+  let validatorsWithoutAvatars = []
+  let orderedValidators = []
   if (validators && validators.length > 1) {
-    return orderBy(
+    orderedValidators = orderBy(
       validators,
       [i => i.description.moniker.toLowerCase()],
       "asc"
     )
-  } else {
-    return []
+    validatorsWithAvatars = orderedValidators.filter(
+      v => (v = v.description.identity)
+    )
+    validatorsWithoutAvatars = orderedValidators.filter(
+      v => (v = !v.description.identity)
+    )
+    orderedValidators = validatorsWithAvatars.concat(validatorsWithoutAvatars)
   }
+  return orderedValidators
 }
